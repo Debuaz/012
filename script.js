be00cc6837b09ea3cbac59865ebccaeaf331ad35
Maksim <barsukienot@gmail.com>
2021-03-15 21:55:30 +0300

    15

--- script.js
+++ script.js
@@ -23,22 +23,108 @@
 
 'use strict';
 
-const numberOFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+// const numberOFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-const personalMovieDB = {
-   count: numberOFilms,
-   movies: {},
-   actors: {},
-   genres: {},
-   privat: false
-};
+// const personalMovieDB = {
+//    count: numberOFilms,
+//    movies: {},
+//    actors: {},
+//    genres: {},
+//    privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фиильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фиильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фиильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фиильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d;
+// personalMovieDB.movies [a] = b;
+// personalMovieDB.movies [c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);  
+
+// 2.Условия (if,else)
+
+
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num< 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// } 
+
+// // Тернарный оператор (?)
+
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+
+// switch Строгое соответствие 
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50: 
+//         console.log('Правдивое');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
+// 2.Циклы
+
+
+// let num = 50;
+
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num < 55);
+
+
+// let num = 50;
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }    
+    
+//     console.log(i);
+// }
+
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
