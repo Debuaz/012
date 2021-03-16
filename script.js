b106b6f93b52243a24a4385c936d62de3d65f3dc
Maksim <barsukienot@gmail.com>
2021-03-16 12:31:48 +0300

    16

--- script.js
+++ script.js
@@ -128,3 +128,38 @@
 "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
 4) Потренироваться и переписать цикл еще двумя способами*/
+
+const numberOFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+   count: numberOFilms,
+   movies: {},
+   actors: {},
+   genres: {},
+   privat: false
+};
+
+for(let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фиильмов?', ''),
+           b = prompt('На сколько оцените его?', '');
+        
+    if (a != null && b !=null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies [a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель"); 
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман"); 
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);  
